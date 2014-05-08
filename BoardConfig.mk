d703b2cc8faa3b101584b0119059a67e204d1594
PlayfulGod <playfulgod@gmail.com>
2014-05-08 01:24:30 -0400

    Prep for Official CWM using prebuilt kernel and dt.img

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,19 +17,20 @@
 include device/lge/g2-common/BoardConfigCommon.mk
 
 ### Temporary until proper changes are added to kernel/lge/msm8974
-#BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x05000000 --dt device/lge/d950/dtb --tags_offset 0x04800000 ## For prebuilt kernel use to bypass g2-common
-#TARGET_PREBUILT_KERNEL := device/lge/d950/kernel ## For initial recovery builds
-#BOARD_KERNEL_SEPARATED_DT := ## For prebuilt kernel use to bypass g2-common
-#BOARD_CUSTOM_BOOTIMG_MK := ## For prebuilt kernel use to bypass g2-common
-TARGET_KERNEL_CONFIG := z-att-perf_defconfig
-TARGET_KERNEL_SOURCE := kernel/lge/d950
+BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x05000000 --dt device/lge/d950/dt.img --tags_offset 0x04800000 ## For prebuilt kernel use to bypass g2-common
+TARGET_PREBUILT_KERNEL := device/lge/d950/kernel ## For initial recovery builds
+BOARD_KERNEL_SEPARATED_DT := ## For prebuilt kernel use to bypass g2-common
+BOARD_CUSTOM_BOOTIMG_MK := ## For prebuilt kernel use to bypass g2-common
+#TARGET_KERNEL_CONFIG := z-att-perf_defconfig
+TARGET_KERNEL_SOURCE := #kernel/lge/d950
 ### End Temporary ###
 
-#TARGET_KERNEL_CONFIG := cyanogenmod_d950_defconfig
+#TARGET_KERNEL_CONFIG := cyanogenmod_d802_defconfig
 
-G2_DTS_TARGET := msm8974-z-att
+#G2_DTS_TARGET := msm8974-z-att
 
 ## Recovery
 BOARD_RECOVERY_SWIPE := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 
-TARGET_OTA_ASSERT_DEVICE := d950,d959,gflex,galbi
+TARGET_OTA_ASSERT_DEVICE := zee
